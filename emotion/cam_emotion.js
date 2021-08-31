b0e21880b9bd001f1cfd733d8f1090bde5f94758
yayaja11 <yayaja11@naver.com>
2021-08-31 10:22:40 +0900

    message

---
+++ emotion/cam_emotion.js
@@ -0,0 +1,118 @@
+/* 
+face-api 적용 코드
+
+face-api 내 모든 모듈이 해당 파일에 맞춰져있어서
+weight(binary) 파일 + manifest(json) 파일을 동일 이름으로 변경 후 사용해야할 듯
+*/
+
+import React, { useState, useEffect, useRef } from "react";
+import * as faceapi from "face-api.js";
+import "./cam_emotion.css"; // 비디오 화면 내 draw 이미지 조정
+import AccountPage from "../account";// detection 전달 
+
+function CamEmotion() {
+  var i = 0
+  var strdata
+  const videoHeight = 240;
+  const videoWidth = 360;
+  const [initializing, setInitializing] = useState(false);
+  const videoRef = useRef();
+  const canvasRef = useRef();
+
+  useEffect(() => {
+    const loadModels = async () => {
+      const MODEL_URL = process.env.PUBLIC_URL + "/models";
+      setInitializing(true);
+      Promise.all([
+        faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+        faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+        faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+        faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL),
+      ]).then(() => {
+        navigator.getUserMedia(
+          {
+            video: {},
+          },
+          (stream) => (videoRef.current.srcObject = stream),
+          errorCallback
+        );
+      });
+    };
+    loadModels();
+  }, []);
+
+  /* parameter 3 - error code 추가 */
+  const errorCallback = function (e) {
+    console.log("Rejected!", e);
+  };
+  // 카메라 작동
+
+  const handleVideoOnplay = () => {
+    setInterval(async () => {
+      if (initializing) {
+        setInitializing(false);
+      }
+      canvasRef.current.innerHTML = faceapi.createCanvasFromMedia(
+        videoRef.current
+      );
+      const displaySize = {
+        width: videoWidth,
+        height: videoHeight,
+      };
+      faceapi.matchDimensions(canvasRef.current, displaySize);
+      const detections = await faceapi
+        .detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions())
+        .withFaceLandmarks()
+        .withFaceExpressions();
+      const resizedDetections = faceapi.resizeResults(detections, displaySize);
+      canvasRef.current
+        .getContext("2d")
+        .clearRect(0, 0, videoWidth, videoHeight);
+      faceapi.draw.drawDetections(canvasRef.current, resizedDetections);
+      faceapi.draw.drawFaceLandmarks(canvasRef.current, resizedDetections);
+      faceapi.draw.drawFaceExpressions(canvasRef.current, resizedDetections);
+
+      // 얼굴이 감지되지 않는 경우는 undefined가 됨(값이 아예 안들어가는듯) - 콘솔 창에서는 에러가 함
+      // DB 저장시, undefined 따로 처리해줘야함 + undefined 지속 시 alert 띄우는 작업 추가
+      console.log(detections[0]); //[0].expressions
+      
+
+      function saveAsFile(str, filename) {
+        
+        var hiddenElement = document.createElement('a');
+        hiddenElement.href = 'data:attachment/text,' + encodeURI(str);
+        hiddenElement.target = '_blank';
+        hiddenElement.download = filename;
+        hiddenElement.click();
+        
+        }
+      i = i +1
+      var strdata = detections[0] + "\n";
+      
+      if (i == 10){
+        saveAsFile(strdata, "output.txt");}
+    }, 100);
+  };
+
+  //전달해주는부분
+  return (
+    <div>
+      
+      <div class="d-flex justify-content-end">
+        <video
+          ref={videoRef}
+          autoPlay
+          muted
+          height={videoHeight}
+          width={videoWidth}
+          onPlay={handleVideoOnplay}
+          
+        />
+       
+        <canvas ref={canvasRef} class="position-absolute" />
+      </div>
+    </div>
+  );
+}
+
+export default CamEmotion;
